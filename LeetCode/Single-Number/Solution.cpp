be712b45020ee3ebcac3f6f33b0eb023cca834cd
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-10-11 14:29:09 +0530

    Add solution for Single Number

---
+++ LeetCode/Single-Number/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorN = 0;
+        for(int i =0;i<nums.size();i++){
+            xorN = xorN ^ nums[i];
+        }
+        return xorN;
+    }
+};
