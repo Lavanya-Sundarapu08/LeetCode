5f640641260a450b0feac53a654bcdce9863fd23
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-10-20 23:24:11 +0530

    Add solution for Final Value of Variable After Performing Operations

--- LeetCode/Final-Value-of-Variable-After-Performing-Operations/Solution.cpp
+++ LeetCode/Final-Value-of-Variable-After-Performing-Operations/Solution.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int n = operations.size();
-        if(operations.empty()) return 0;
-        int fnal = 0;
-        int X =0;
-        for(int i=0;i<n;i++){
-            if(operations[i]=="--X"){
-                fnal--;
-                X=X+fnal;
+        int x =0;
+        for(auto i:operations){
+            if(i == "X++" || i=="++X"){
+                x++;
             }
-            else if(operations[i]=="X--"){
-                X = X + fnal;
-                fnal--;
-            }
-            else if(operations[i]=="++X"){
-                fnal++;
-                X = X+fnal;
-            }
-            else{
-                X= X+fnal;
-                fnal++;
-            }
+            else x--;
         }
-        return fnal;
+        return x;
     }
 };
