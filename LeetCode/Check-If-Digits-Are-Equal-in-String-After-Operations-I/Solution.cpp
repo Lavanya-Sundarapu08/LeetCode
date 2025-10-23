fabcdb6a15a9e2a557aa7af1bd1bbebf83f34014
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-10-23 21:19:34 +0530

    Add solution for Check If Digits Are Equal in String After Operations I

--- LeetCode/Check-If-Digits-Are-Equal-in-String-After-Operations-I/Solution.cpp
+++ LeetCode/Check-If-Digits-Are-Equal-in-String-After-Operations-I/Solution.cpp
@@ -1,2 +1,17 @@
-"4867716181911413979960155821878"
-"472371665226339635424494604455723348326857778972516313101962164"
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.size() > 2) {
+        string temp = "";
+        for (size_t i = 0; i < s.size() - 1; i++) {
+            int a = s[i] - '0';
+            int b = s[i + 1] - '0';
+            int sum = (a + b) % 10;  // only keep the last digit
+            temp += to_string(sum);
+        }
+        s = temp;
+    }
+    // when only 2 digits left
+    return (s[0] == s[1]);
+    }
+};
