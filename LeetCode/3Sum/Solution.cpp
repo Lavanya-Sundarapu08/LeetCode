c7b4b6eb36e4676a2f880fef231bbad99c68cb4e
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-11-03 21:10:30 +0530

    Add solution for 3Sum

--- LeetCode/3Sum/Solution.cpp
+++ LeetCode/3Sum/Solution.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-    sort(nums.begin(), nums.end());
-    int N = nums.size();
-     int  K=0;
-    for (int i = 0; i < N - 2; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates for i
-       
-        int left = i + 1;
-        int right = N - 1;
+        vector<vector<int>> ans;
+        int n =nums.size();
+        int target= 0;
+        sort(nums.begin(),nums.end());
+        for(int i =0;i<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+              long long sum = nums[i];
+              sum += nums[j];   
+              sum += nums[k];
+              if(sum == target){
+                vector<int> temp = {nums[i],nums[j],nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
 
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
-            if (sum == K) {
-                res.push_back({nums[i], nums[left], nums[right]});
-                left++;
-                right--;
-                // Skip duplicates for left
-                while (left < right && nums[left] == nums[left - 1]) left++;
-                // Skip duplicates for right
-                while (left < right && nums[right] == nums[right + 1]) right--;
-            } else if (sum < K) {
-                left++;
-            } else {
-                right--;
+                // there should be while loop inside 
+                while(j<k && nums[j] == nums[j-1]) j++;
+                while(j<k && nums[k] == nums[k+1]) k--;
+              }
+              else if(sum < target) j++;
+              else k--;
             }
         }
-    }
-
-    return res;
+        return ans;
     }
 };
