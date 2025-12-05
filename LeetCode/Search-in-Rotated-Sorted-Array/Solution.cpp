167075834be99b18c522737d50b7a271909a820b
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-12-05 19:42:55 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
@@ -1,11 +1,27 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int n= nums.size();
-        for (int i = 0; i < n; i++) {
-        if (nums[i] == target)
-            return i;
-        }
-        return -1;
-    }
-};
+1class Solution {
+2public:
+3    int search(vector<int>& nums, int target) {
+4        int n = nums.size();
+5        int low = 0 , high = n-1;
+6        while(low <= high){
+7            int mid = (low+high) /2;
+8            if(nums[mid] == target) return mid;
+9            //left sorted array
+10            else if(nums[low]<= nums[mid]){
+11                if(nums[low]<= target && target <= nums[mid]){
+12                    high = mid-1;
+13                }else{
+14                    low = mid+1;
+15                }
+16            }//right sorted array
+17            else{
+18                if(nums[high]>= target && target >=nums[mid] ){
+19                    low = mid+1;
+20                }else{
+21                    high = mid-1;
+22                }
+23            }
+24        }
+25        return -1;
+26    }
+27};
