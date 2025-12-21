84c80b0b89329aa5d9d6b1b840ead48dab105498
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-12-21 17:02:33 +0530

    Add solution for Concatenation of Array

--- LeetCode/Concatenation-of-Array/Solution.cpp
+++ LeetCode/Concatenation-of-Array/Solution.cpp
@@ -1,11 +1,15 @@
-static int list_append(PyListObject *self, PyObject *v)
-{
-    if (self->allocated <= Py_SIZE(self)) {
-        if (list_resize(self, more) == -1)
-            return -1;
-    }
-    Py_INCREF(v);
-    Py_SET_SIZE(self, Py_SIZE(self)+1);
-    PyList_SET_ITEM(self, Py_SIZE(self)-1, v);
-    return 0;
-}
+1class Solution {
+2public:
+3    vector<int> getConcatenation(vector<int>& nums) {
+4        vector<int> ans;
+5        int n= nums.size();
+6
+7        for(int i =0;i<n;i++){
+8            ans.push_back(nums[i]);
+9        }
+10        for(int i =0;i<n;i++){
+11            ans.push_back(nums[i]);
+12        }
+13        return ans;
+14    }
+15};
