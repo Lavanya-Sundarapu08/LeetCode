8f2dfd8bbde1f2765bdf89431fb4d16833c8bfa3
Lavanya Sundarapu <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2025-12-21 17:09:01 +0530

    Add solution for Concatenation of Array

--- LeetCode/Concatenation-of-Array/Solution.cpp
+++ LeetCode/Concatenation-of-Array/Solution.cpp
@@ -1,15 +1,13 @@
 1class Solution {
 2public:
 3    vector<int> getConcatenation(vector<int>& nums) {
-4        vector<int> ans;
-5        int n= nums.size();
+4        int n= nums.size();
+5        vector<int> ans(2*n);
 6
 7        for(int i =0;i<n;i++){
-8            ans.push_back(nums[i]);
-9        }
-10        for(int i =0;i<n;i++){
-11            ans.push_back(nums[i]);
-12        }
-13        return ans;
-14    }
-15};
+8            ans[i] = nums[i];
+9            ans[i+n] = nums[i];
+10        }
+11        return ans;
+12    }
+13};
