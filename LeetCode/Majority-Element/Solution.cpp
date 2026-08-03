f41065d3166a578f0091af3ab87a5a92b1ac054f
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-03 23:06:33 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.cpp
+++ LeetCode/Majority-Element/Solution.cpp
@@ -1,7 +1,17 @@
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4        sort(nums.begin(),nums.end());
-5        return nums[ nums.size()/2];
-6    }
-7};
+4        int count = 0;
+5        int candidate = 0;
+6
+7        for(int x:nums){
+8            if(count==0){
+9                candidate=x;
+10            }
+11
+12            if(x == candidate) count++;
+13            else count--;
+14        }
+15        return candidate;
+16    }
+17};
