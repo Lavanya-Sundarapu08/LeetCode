57cb038f877e3a8c1aadb0b8368e1d2561860fc9
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-04 22:09:37 +0530

    Add solution for Longest Common Prefix

--- LeetCode/Longest-Common-Prefix/Solution.java
+++ LeetCode/Longest-Common-Prefix/Solution.java
@@ -1,15 +1,13 @@
-1class Solution {
-2public:
-3    string longestCommonPrefix(vector<string>& strs) {
-4        if(strs.empty()) return "";
-5        int n = strs.size();
-6        sort(strs.begin(),strs.end());
-7        string first = strs.front();
-8        string second = strs.back();
-9        int i = 0;
-10        while(i<first.size() && i<second.size() && first[i]==second[i]){
-11            i++;
-12        }
-13        return first.substr(0,i);
-14    }
-15};
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix=[]
+        num = len(strs)
+        for x in zip(*strs):
+            if len(set(x)) == 1:
+                prefix.append(x[0])
+            else:
+                break
+        return "".join(prefix) ```
