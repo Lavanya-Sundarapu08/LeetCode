ba5e150c18cbc9224568ab7c413f3d623afdcdf6
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-04 22:03:19 +0530

    Add solution for Maximum Product of Three Numbers

--- LeetCode/Maximum-Product-of-Three-Numbers/Solution.cpp
+++ LeetCode/Maximum-Product-of-Three-Numbers/Solution.cpp
@@ -1,12 +1,10 @@
 1class Solution {
 2public:
 3    int maximumProduct(vector<int>& nums) {
-4        sort(nums.begin(), nums.end());
-5        int n = nums.size();
-6        
-7        int option1 = nums[n-1] * nums[n-2] * nums[n-3];     // 3 largest
-8        int option2 = nums[0] * nums[1] * nums[n-1];         // 2 smallest + largest
-9        
-10        return max(option1, option2);
-11    }
-12};
+4        int n = nums.size();
+5        sort(nums.begin(),nums.end());
+6        return max(nums[n-1] * nums[n-2] * nums[n-3],
+7                    nums[0] * nums[1] * nums[n-1]);
+8
+9    }
+10};
