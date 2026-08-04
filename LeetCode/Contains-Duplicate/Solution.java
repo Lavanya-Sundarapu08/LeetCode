21d7237566131efd2d2676f26a7c85f69edbf099
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-04 22:11:03 +0530

    Add solution for Contains Duplicate

---
+++ LeetCode/Contains-Duplicate/Solution.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public boolean containsDuplicate(int[] nums) {
+3        Arrays.sort(nums);
+4        for(int i=0;i<nums.length-1;i++){
+5            if(nums[i]==nums[i+1]){
+6                return true;
+7            }
+8            
+9        }
+10        return false;
+11    }
+12}
