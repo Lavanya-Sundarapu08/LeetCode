069b98a12ca974b4caadcfc68c0537a5fd47a8d0
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-05 19:20:34 +0530

    Add solution for Next Greater Element II

--- LeetCode/Next-Greater-Element-II/Solution.cpp
+++ LeetCode/Next-Greater-Element-II/Solution.cpp
@@ -1,23 +1,28 @@
-Stack<Integer> stack = new Stack<>();
-
-public int[] nextGreaterElements(int[] nums) {
-
-    int[] res = new int[nums.length];
-    for(int i=0;i<nums.length;i++) {
-        while(!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-            res[stack.pop()] = nums[i];
-        }
-        stack.push(i);
-    }
-    
-    for(int i=0;i<nums.length;i++) {
-        while(!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-            res[stack.pop()] = nums[i];
-        }
-    }
-
-    while(!stack.isEmpty()) {
-        res[stack.pop()] = -1;
-    }
-    return res;
-}
+1class Solution {
+2public:
+3    vector<int> nextGreaterElements(vector<int>& nums) {
+4        
+5        int n = nums.size();
+6
+7        vector<int> nge(n);
+8
+9        stack<int> st;
+10
+11        for(int i =2*n-1;i>=0;i--){
+12            int idx = i%n;
+13            int currElement = nums[idx];
+14
+15            while(!st.empty() && st.top() <= currElement){
+16                st.pop();
+17            }
+18
+19            if(i < n){
+20                if(st.empty()) nge[i] = -1;
+21                else nge[i] = st.top();
+22            }
+23
+24            st.push(currElement);
+25        }
+26        return nge;
+27    }
+28};
