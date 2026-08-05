4cf416ae3200b6d3ca3a3a4b5156430d1eb40099
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-05 11:37:50 +0530

    Add solution for Reverse Integer

--- LeetCode/Reverse-Integer/Solution.cpp
+++ LeetCode/Reverse-Integer/Solution.cpp
@@ -1,7 +1,13 @@
-// pop operation:
-pop = x % 10;
-x /= 10;
-
-// push operation:
-temp = rev * 10 + pop;
-rev = temp;
+1class Solution:
+2    def reverse(self, x: int) -> int:
+3        sign = -1 if x < 0 else 1
+4        x *= sign
+5        rev = 0
+6
+7        while x:
+8            rev = rev * 10 + x % 10
+9            x //= 10
+10
+11        rev *= sign
+12
+13        return rev if -2**31 <= rev <= 2**31 - 1 else 0
