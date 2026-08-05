ff2db728a4dad2846a4d2033e0991e0b2aa99caf
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-05 10:40:38 +0530

    Add solution for Ugly Number

--- LeetCode/Ugly-Number/Solution.cpp
+++ LeetCode/Ugly-Number/Solution.cpp
@@ -1,11 +1,17 @@
 1class Solution {
 2public:
 3    bool isUgly(int n) {
-4        if(n<=0) return false;
-5        while(n%2==0) n/=2;
-6        while(n%3==0) n/=3;
-7        while(n%5==0) n/=5;
+4        if (n <= 0) return false;
+5
+6        while (n % 2 == 0)
+7            n /= 2;
 8
-9        return n==1;
-10    }
-11};
+9        while (n % 3 == 0)
+10            n /= 3;
+11
+12        while (n % 5 == 0)
+13            n /= 5;
+14
+15        return n == 1;
+16    }
+17};
