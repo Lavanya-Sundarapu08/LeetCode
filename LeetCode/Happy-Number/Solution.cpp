ab65903ee4582301f05ee6248b9b7fdfea770c8f
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-05 10:07:22 +0530

    Add solution for Happy Number

--- LeetCode/Happy-Number/Solution.cpp
+++ LeetCode/Happy-Number/Solution.cpp
@@ -1,26 +1,26 @@
 1class Solution {
 2public:
 3
-4    int getSquareSum(int n) {
-5        int sum = 0;
-6        while (n > 0) {
-7            int digit = n % 10;
-8            sum += digit * digit;
+4    int findSum(int n){
+5        int sum =0;
+6        while(n >0){
+7            int d = n % 10;
+8            sum+= d * d;
 9            n /= 10;
 10        }
 11        return sum;
 12    }
-13    
-14    bool isHappy(int n) {
-15        int slow = n;
-16        int fast = getSquareSum(n);
+13    bool solve( int n, unordered_set<int>& seen){
+14        if( n == 1) return true;
+15
+16        if( seen.count(n) ) return false;
 17
-18        // Floyd’s cycle detection
-19        while (fast != 1 && slow != fast) {
-20            slow = getSquareSum(slow);
-21            fast = getSquareSum(getSquareSum(fast));
-22        }
-23
-24        return fast == 1;
+18        seen.insert(n);
+19
+20        return solve(findSum(n), seen);
+21    }
+22    bool isHappy(int n) {
+23        unordered_set<int> seen;
+24        return solve(n,seen);
 25    }
 26};
