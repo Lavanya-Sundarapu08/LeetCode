11dba4ba3856960a1948873d58b8dd5854bf1117
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-06 10:04:55 +0530

    Add solution for Rotate Image

--- LeetCode/Rotate-Image/Solution.cpp
+++ LeetCode/Rotate-Image/Solution.cpp
@@ -1,14 +1,14 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for(int i = 0 ;i<=n-2;i++){
-            for(int j= i+1;j<=n-1;j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-        for(int i = 0;i<=n-1;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
-    }
-};
+1class Solution {
+2public:
+3    void rotate(vector<vector<int>>& matrix) {
+4        int n = matrix.size();
+5        for(int i = 0 ;i<=n-2;i++){
+6            for(int j= i+1;j<=n-1;j++){
+7                swap(matrix[i][j],matrix[j][i]);
+8            }
+9        }
+10        for(int i = 0;i<=n-1;i++){
+11            reverse(matrix[i].begin(),matrix[i].end());
+12        }
+13    }
+14};
