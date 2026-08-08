337974cdaf0427d017fb0ef01d0cb483cbda89bc
Sundarapu Lavanya Suryasri Devika <147488022+Lavanya-Sundarapu08@users.noreply.github.com>
2026-08-08 21:36:37 +0530

    Add solution for Largest Rectangle in Histogram

--- LeetCode/Largest-Rectangle-in-Histogram/Solution.cpp
+++ LeetCode/Largest-Rectangle-in-Histogram/Solution.cpp
@@ -1,31 +1,47 @@
 1class Solution {
 2public:
 3    int largestRectangleArea(vector<int>& heights) {
-4        stack<int> st;
-5        int maxArea = 0;
+4        int n = heights.size();
+5        stack<int> st;
 6
-7        int n = heights.size();
+7        int leftsmall[n], rightsmall[n];
 8
-9        for(int i =0;i<=n;i++){
-10
-11            while(!st.empty() && (i == n || heights[st.top()] >= heights[i])){
-12                int heightA= heights[st.top()];
+9        // Compute Nearest Smaller to Left (NSL) for each bar
+10        for (int i = 0; i < n; i++) {
+11            // Pop until a smaller element is found or stack is empty
+12            while (!st.empty() && heights[st.top()] >= heights[i]) {
 13                st.pop();
-14
-15                int width;
-16                if(st.empty()){
-17                    width = i;
+14            }
+15
+16            // If stack is empty, no smaller element on the left
+17            leftsmall[i] = st.empty() ? 0 : st.top() + 1;
 18
-19                }else{
-20                    width = i - st.top() - 1;
-21
-22                }
-23
-24                maxArea = max(maxArea, width * heightA);
-25            }
-26            st.push(i);
-27        }
-28        return maxArea;
-29
-30    }
-31};
+19            // Push current index to stack
+20            st.push(i);
+21        }
+22
+23        // Clear stack to reuse for NSR
+24        while (!st.empty()) st.pop();
+25
+26        // Compute Nearest Smaller to Right (NSR) for each bar
+27        for (int i = n - 1; i >= 0; i--) {
+28            while (!st.empty() && heights[st.top()] >= heights[i]) {
+29                st.pop();
+30            }
+31
+32            // If stack is empty, no smaller to the right
+33            rightsmall[i] = st.empty() ? n - 1 : st.top() - 1;
+34
+35            st.push(i);
+36        }
+37
+38        // Calculate max area using NSL and NSR
+39        int maxA = 0;
+40        for (int i = 0; i < n; i++) {
+41            int width = rightsmall[i] - leftsmall[i] + 1;
+42            maxA = max(maxA, heights[i] * width);
+43        }
+44
+45        return maxA;
+46    }
+47};
